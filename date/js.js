036527d48349072e6c75d0ef8781d173189e141b
Sumit-Sharma00 <142771342+Sumit-Sharma00@users.noreply.github.com>
2024-07-15 11:30:25 +0530

    Add files via upload

---
+++ date/js.js
@@ -0,0 +1,12 @@
+var d= document.getElementById("no");
+d.addEventListener('mouseover',move)
+function move()
+{
+    d.classList.toggle("move");
+     d.classList.toggle("left");
+    // d.classList.toggle("right");
+     d.classList.toggle("top");
+    //  d.classList.toggle("bottom");
+
+}
+
